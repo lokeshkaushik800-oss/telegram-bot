443b772a07e6dd50c6cc8350311425f2ce7c584b
lokeshkaushik800-oss <lokeshkaushik800@gmail.com>
2026-03-14 13:37:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,43 +2,47 @@
 
 include "config.php";
 
-$api = "https://api.telegram.org/bot$bot_token/";
+$update = json_decode(file_get_contents("php://input"), true);
 
-$update = json_decode(file_get_contents("php://input"),true);
+if(isset($update["message"])){
 
-$message = $update["message"] ?? null;
-$callback = $update["callback_query"] ?? null;
+$chat_id = $update["message"]["chat"]["id"];
+$text = $update["message"]["text"];
 
-$chat_id = $message["chat"]["id"] ?? null;
-$text = $message["text"] ?? "";
-
-$data = $callback["data"] ?? null;
-$cid = $callback["message"]["chat"]["id"] ?? null;
-
-function sendMessage($chat,$msg,$keyboard=null){
-global $api;
-
-$params=[
-"chat_id"=>$chat,
-"text"=>$msg
-];
-
-if($keyboard){
-$params["reply_markup"]=json_encode($keyboard);
 }
 
-file_get_contents($api."sendMessage?".http_build_query($params));
+function sendMessage($chat_id,$text,$keyboard=null){
+
+global $bot_token;
+
+$url = "https://api.telegram.org/bot$bot_token/sendMessage";
+
+$data = [
+"chat_id"=>$chat_id,
+"text"=>$text,
+"reply_markup"=>$keyboard
+];
+
+$options = [
+"http"=>[
+"header"=>"Content-Type: application/json",
+"method"=>"POST",
+"content"=>json_encode($data)
+]
+];
+
+$context = stream_context_create($options);
+
+file_get_contents($url,false,$context);
 
 }
 
 if($text=="/start"){
 
-file_put_contents("users.txt",$chat_id."\n",FILE_APPEND);
-
-$keyboard=[
+$keyboard = json_encode([
 "inline_keyboard"=>[
 [
-["text"=>"Buy Premium","callback_data"=>"premium"]
+["text"=>"Get Premium","callback_data"=>"premium"]
 ],
 [
 ["text"=>"Premium Demo","url"=>$demo_channel]
@@ -47,7 +51,7 @@ $keyboard=[
 ["text"=>"How To Get Premium","url"=>$show_channel]
 ]
 ]
-];
+]);
 
 sendMessage($chat_id,"Price ₹99\nValidity Lifetime",$keyboard);
 
